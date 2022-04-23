52cf01baa1e39a41c3d2236fa0b3b82edda714be
diegobaena89 <diegobf.cello@gmail.com>
2022-04-23 13:38:00 -0300

    page to create post added

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -67,13 +67,36 @@ export const useAuthentication = () => {
   };
 
   // logout - signout
-  const logout =  () => {
-
+  const logout = () => {
     checkIfIsCancelled();
 
     signOut(auth);
+  };
+  // login - sign in
+  const login = async (data) => {
+    checkIfIsCancelled();
 
-  }
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-foun")) {
+        systemErrorMessage = "Usuário não encontrado";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro. Por favor, tente mais tarde.";
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     return () => setCancelled(true);
@@ -84,6 +107,7 @@ export const useAuthentication = () => {
     createUser,
     error,
     loading,
-    logout
+    logout,
+    login,
   };
 };
